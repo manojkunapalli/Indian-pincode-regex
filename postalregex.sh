82105fcc91a15ab19d9439e804683196b246ff57
manojkunapalli <manojkunapalli@gmail.com>
2020-10-02 00:25:39 +0530

    Added: pincode regex

--- postalregex.sh
+++ postalregex.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 echo "welcome to postal Index Regex"
+echo -e
+
+pattern="^[1-9]{1}[0-9]{5}$"
+
+read -p "Enter postalcode = " pincode
+
+if [[ $pincode =~ $pattern ]]
+then
+	echo "entered pincode is 6 digit number"
+else
+	echo "pincode doesn't match pattern"
+fi
+
